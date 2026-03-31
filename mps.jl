edf6479ea25e85569d50a8834a4129c2921af4da
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-03-31 13:42:24 +0200

    Implement MPS with ITensor

---
+++ mps.jl
@@ -0,0 +1,27 @@
+using ITensorMPS
+using ITensors
+
+N = 8 # Number of qubits
+
+println("""Running stabilizer simulation on $N qubits starting at |0⟩|0⟩…|0⟩.
+        Hadamard-CNOT circuit.
+        """)
+
+sites = siteinds("Qubit", N) # Array of one physical index per site
+states = ["↑" for n in 1:N] # Array of single-qubit states
+ψ = MPS(sites, states) # MPS with site indices, in product state |0⟩|0⟩…|0⟩
+
+circuit = ITensor[]
+
+# Each push! adds an operator at the end of the circuit
+push!(circuit, op("H", sites[1]))
+push!(circuit, op("H", sites[3]))
+push!(circuit, op("H", sites[5]))
+push!(circuit, op("H", sites[7]))
+
+push!(circuit, op("CNOT", sites[1], sites[2]))
+push!(circuit, op("CNOT", sites[3], sites[4]))
+push!(circuit, op("CNOT", sites[5], sites[6]))
+push!(circuit, op("CNOT", sites[7], sites[8]))
+
+ψ = apply(circuit, ψ)
